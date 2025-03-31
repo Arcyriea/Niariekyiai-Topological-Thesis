b917a422a21fab92a792b208157a4b22f7288b79
SubsectorKirisakyia <108776309+SubsectorKirisakyia@users.noreply.github.com>
2025-03-31 09:49:22 +0700

    Initialize development for 3D portfolio React App

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+
+const App = () => {
+  return (
+    <>
+    <center>
+      <h1>Niariekyiai's Model Portfolio</h1>
+    <Canvas>
+
+    </Canvas>
+    </center>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/ModelLoader.jsx
@@ -0,0 +1,19 @@
+import { useLoader } from '@react-three/fiber';
+import './App.css'
+
+const ModelLoader = (filePath) => {
+    const gltf = useLoader(GLTFLoader, filePath);
+  
+    if (gltf) {
+      return (
+        <primitive object={gltf.scene} />
+      );
+    }
+  
+    return (
+      <mesh ref={meshRef} scale={[10, 10, 10]} position={[0, 0, 0]}>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshBasicMaterial color="gray" />
+      </mesh>
+    );
+  };
