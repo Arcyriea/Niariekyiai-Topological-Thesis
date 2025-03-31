2f55e9f85d28e5d4717ee5c985400752a93d51b6
SubsectorKirisakyia <108776309+SubsectorKirisakyia@users.noreply.github.com>
2025-03-31 13:02:36 +0700

    Adding model file in, about to test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
 import './App.css'
+import { ModelLoader } from "./assets/ModelLoader"
 import { Canvas } from '@react-three/fiber'
 
 const App = () => {
   return (
     <>
-    <center>
-      <h1>Niariekyiai's Model Portfolio</h1>
-    <Canvas>
-
-    </Canvas>
-    </center>
+    <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
+      <h1>Nianiekryai's Model Portfolio</h1>
+      <Canvas>
+        <ModelLoader filepath="assets/models/Kantogawa Assault Battlecruiser.glb?url" />
+      </Canvas>
+    </div>
     </>
   )
 }
